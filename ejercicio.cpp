f065044b3160cdfc940cb337abaff8a3a8ebde46
Jorge Dueñas <jorge@iMac-de-Jorge.local>
2020-04-07 17:05:16 -0500

    subi archivo ejercicio.cpp

---
+++ ejercicio.cpp
@@ -0,0 +1,130 @@
+ /
+ //  main.cpp
+ //  practica 1
+ //
+ //  Created by Jorge Dueñas on 4/7/20.
+ //  Copyright © 2020 lucia Dueñas. All rights reserved.
+ //
+ /*
+ #include <iostream>
+ #include <string.h>
+
+ using namespace std;
+
+ int main(int argc, const char * argv[]) {
+     string nombre;
+     string telef;
+     string direc;
+     string hob;
+     int e=0;
+     int t=0;
+     
+     cout<<"ingresa tu nombre: ";cin>>nombre;
+     cout<<"ingresa tu edad: ";cin>>e;
+     cout<<"ingresa tu telefono: ";cin>>t;
+     cout<<"ingresa tu direccion: ";cin>>direc;
+     cout<<"ingresa tu hobbie: ";cin>>hob;
+   
+     
+     cout<<"* "<<nombre<<endl;
+     cout<<"*"<<e<<endl;
+     cout<<"*"<<t<<endl;
+     cout<<"*"<<direc<<endl;
+     cout<<"*"<<hob<<endl;
+     
+     
+     
+
+     return 0;
+ }
+  
+  
+
+
+ #include <iostream>
+ #include <math.h>
+ using namespace std;
+
+ int main(int argc, const char * argv[]) {
+     float a ,b,su=0,re=0,mul=0,div=0,ra=0;
+     cout<<"escriba un numero: ";cin>>a;
+     cout<<"escriba otro numero: ";cin>>b;
+     su=a+b;
+     re=a-b;
+     mul=a*b;
+     div=a/b;
+     ra=sqrt(a);
+     re=sqrt(b);
+     
+     cout<<"su suma es: "<<su<<endl;
+     cout<<"su resta es: "<<re<<endl;
+     cout<<"su multiplicacion es: "<<mul<<endl;
+     cout<<"su division es: "<<div<<endl;
+     cout<<"su raiz cuadrada del primero es: "<<ra<<endl;
+     cout<<"su raiz cuadrada del segundo es: "<<re<<endl;
+     
+     return 0;
+     
+ }
+  
+
+ # include<iostream>
+ using namespace std;
+
+ int main() {
+     float r=0,pi=3.14,ba=0,ha=0,ac=0,at=0;
+     
+     cout<<"De escriba el radio de su circulo:";cin>>r;
+     cout<<"De escriba la base de su triangulo:";cin>>ba;
+     cout<<"De escriba ela altura de tu triangulo:";cin>>ha;
+     
+     ac=pi*r;
+     at=(ba*ha)/2;
+     cout<<"el area de su circulo es: "<<ac<<endl;
+     cout<<"el area de su circulo es: "<<at<<endl;
+     
+     
+     return 0;
+ }
+
+ //extra
+ #include<iostream>
+ #include<math.h>
+ using namespace std;
+ int main(){
+     
+     float a,b,c,ate=0,ati=0,ates=0,s=0;
+     cout<<"ponga el lado a: ";cin>>a;
+     cout<<"ponga el lado b: ";cin>>b;
+     cout<<"ponga el lado c: ";cin>>c;
+     
+     if(a==b and a==c){
+         ate=(sqrt(3)/4)*(pow(a,2));
+         cout<<"el area de tu triangulo equilatero es: "<<ate<<endl;
+     }
+     else
+     {
+         if(a==b  and a!=c and b!=c)
+         {
+             ati=(b*(sqrt(pow(a,2)-(pow(b,2)/4))))/2;
+             cout<<"el area de tu triangulo isosceles es: "<<ati<<endl;
+         }
+         else
+         {
+             if(a!=b and a!=c and c!= b)
+             {
+                 s=(a+b+c)/2;
+                 ates=sqrt(s*(s-a)*(s-b)*(s-c));
+                 cout<<"el area de tu triangulo escaleno es: "<<ates<<endl;
+             }
+         }
+         
+     }
+     
+     
+     
+     return 0;
+ }
+ */
+
+
