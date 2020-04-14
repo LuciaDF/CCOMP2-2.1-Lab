3b843dff28cb070e7a51bdae029fa18023793c5c
Jorge Dueñas <jorge@iMac-de-Jorge.local>
2020-04-14 10:24:38 -0500

    ingrezo nuevo codigo ejercicios 2

---
+++ hello world/Practica_libro/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  hello world
+//
+//  Created by Jorge Dueñas on 4/13/20.
+//  Copyright © 2020 lucia Dueñas. All rights reserved.
+//
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main (int argc, const char * argv[]){
+    int tamaño =5;
+    char nombre[tamaño];
+    cout<<"digite su nombre"<<endl;cin>>nombre;
+    
+    for(int i=0; i<tamaño;i++){
+        cout<<nombre[1]<<endl;
+        
+    }
+    
+       
+
+    return 0;
+    
+}
